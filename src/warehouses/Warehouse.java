b2be2fc7418892c17dbaefe1aca4d9b0e1bdc993
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2023-01-03 00:01:45 +0500

    Implemented Warehouse.java

--- src/warehouses/Warehouse.java
+++ src/warehouses/Warehouse.java
@@ -1,5 +1,24 @@
 package warehouses;
 
 public class Warehouse {
-    //TODO реализовать класс
+    private String warehouseName;
+
+    public Warehouse(String warehouseName) {
+        this.warehouseName = warehouseName;
+    }
+
+    public String getWarehouseName() {
+        return warehouseName;
+    }
+
+    public void setWarehouseName(String warehouseName) {
+        this.warehouseName = warehouseName;
+    }
+
+    @Override
+    public String toString() {
+        return "Warehouse{" +
+                "warehouseName='" + warehouseName + '\'' +
+                '}';
+    }
 }
