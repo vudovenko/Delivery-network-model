93708b8f997603c4f02f8408bf44ac78784326b5
Andrey Dikov <DikovAndrey856@gmail.com>
2023-01-02 20:14:29 +0300

    Create store class

---
+++ src/Store.java
@@ -0,0 +1,23 @@
+public class Store {
+    private Boolean isDemandSatisfied;
+
+    public Store(Boolean isDemandSatisfied) {
+        this.isDemandSatisfied = isDemandSatisfied;
+    }
+
+    public Boolean getDemandSatisfied() {
+        return isDemandSatisfied;
+    }
+
+    public void setDemandSatisfied(Boolean demandSatisfied) {
+        isDemandSatisfied = demandSatisfied;
+    }
+
+    public void showStatus() {
+        if (isDemandSatisfied) {
+            System.out.println("Спрос магазина удовлетворен");
+        } else {
+            System.out.println("Спрос магазина пока еще не удовлетвоерен");
+        }
+    }
+}
