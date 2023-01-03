216f156a0dcaf03b8e0eb134818165fb321b14b1
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2023-01-03 20:08:05 +0500

    completed the parking class

--- src/warehouses/ParkingSpace.java
+++ src/warehouses/ParkingSpace.java
@@ -1,5 +1,38 @@
 package warehouses;
 
+import cars.Car;
+
+import java.util.LinkedList;
+
 public class ParkingSpace {
-    //TODO делай!
+
+    private LinkedList<Car> parkingQueue;
+    private Integer parkingSize;
+
+    public ParkingSpace() {
+        parkingQueue = new LinkedList<>();
+        parkingSize = 0;
+    }
+
+    public void addCarToParkingSpace(Car car) {
+        parkingQueue.offerLast(car);
+        parkingSize++;
+    }
+
+    public void removeCarFromParkingSpace() {
+        parkingQueue.pollFirst();
+        parkingSize--;
+    }
+
+    public Integer getParkingSize() {
+        return parkingSize;
+    }
+
+    @Override
+    public String toString() {
+        return "ParkingSpace{" +
+                "parkingQueue=" + parkingQueue +
+                ", parkingSize=" + parkingSize +
+                '}';
+    }
 }
