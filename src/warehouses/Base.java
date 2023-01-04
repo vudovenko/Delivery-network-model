bc8c8dd556f9b908a8a904cd28d3490ad51a6e40
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2023-01-04 21:16:43 +0500

    Appended Base.java

--- src/warehouses/Base.java
+++ src/warehouses/Base.java
@@ -1,9 +1,48 @@
 package warehouses;
 
 import towns.OrderRequest;
+import towns.TypeProduct;
+
+import java.util.List;
 
 public class Base {
-    private void getProductRequest(OrderRequest orderRequest) {
 
+    private List<Warehouse> warehouses;
+
+    public Base(List<Warehouse> warehouses) {
+        this.warehouses = warehouses;
+    }
+
+    public List<Warehouse> getWarehouses() {
+        return warehouses;
+    }
+
+    public void setWarehouses(List<Warehouse> warehouses) {
+        this.warehouses = warehouses;
+    }
+
+    private static void getProductRequest(OrderRequest orderRequest) {
+        System.out.printf("База получила запрос из города \"" + orderRequest.getTown()
+                + "\" от магазина №" + orderRequest.getShopId()
+                + " на товар \"" + orderRequest.getTypeProduct() + "\"");
+
+    }
+
+    private Warehouse findSuitableWarehouse(TypeProduct typeProduct) {
+        for (Warehouse warehouse : warehouses) {
+            if (warehouse.getProduct().getTypeProduct().equals(typeProduct)) {
+                /*
+                * todo
+                *  доделать поиск склада*/
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Base{" +
+                "warehouses=" + warehouses +
+                '}';
     }
 }
