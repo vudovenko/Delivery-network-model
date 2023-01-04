2fb4e6fd77beb2c83e8dca1e551aab1c90ad992d
Andrey Dikov <DikovAndrey856@gmail.com>
2023-01-04 19:20:32 +0300

    modified Town class

--- src/towns/Town.java
+++ src/towns/Town.java
@@ -1,17 +1,18 @@
 package towns;
 
 import interfaces.DemonstratingStatus;
+import mainThreads.Main;
+import mainThreads.TimeClass;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Town implements DemonstratingStatus {
     private List<Store> stores;
     private Integer distanceToBase;
-    private Double demand;
+    private Integer demand;
 
-    public Town(Integer numberStores, Integer distanceToBase, Double demand) {
-        for (int i = 0; i < numberStores; ++i) {
+    public Town(Integer numberStores, Integer distanceToBase, Integer demand) {
+        for (int i = 1; i <= numberStores; ++i) {
             stores.add(new Store(this, i));
         }
         this.distanceToBase = distanceToBase;
@@ -34,12 +35,37 @@ public class Town implements DemonstratingStatus {
         this.distanceToBase = distanceToBase;
     }
 
-    public Double getDemand() {
+    public Integer getDemand() {
         return demand;
     }
 
-    public void setDemand(Double demand) {
+    public void setDemand(Integer demand) {
         this.demand = demand;
+    }
+
+    public void generateRequest() {
+        try {
+            Thread.sleep(Main.randomNumberInRange(0, demand * 24 * TimeClass.VIRTUAL_HOUR));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        OrderRequest orderRequest = new OrderRequest(this,
+                Main.randomNumberInRange(0, stores.size()),
+                generateRandomTypeProduct());
+        //Base.getProductRequest(orderRequest);
+    }
+
+    public TypeProduct generateRandomTypeProduct() {
+        int randomNumber = Main.randomNumberInRange(1, 4);
+        if (randomNumber == 1) {
+            return TypeProduct.HEAVY_OVERSIZE;
+        } else if (randomNumber == 2) {
+            return TypeProduct.HEAVY_EURO_PALLETS;
+        } else if (randomNumber == 3) {
+            return TypeProduct.PLUMBING;
+        } else {
+            return TypeProduct.LIGHT_EURO_PALLETS;
+        }
     }
 
     @Override
