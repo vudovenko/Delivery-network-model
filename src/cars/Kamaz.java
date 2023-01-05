c8ebfd7657f2bf9533c2dd74ecddfbee1f9f87b3
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2023-01-05 13:56:24 +0500

    added timeIntWarehouse in cars and toString()

--- src/cars/Kamaz.java
+++ src/cars/Kamaz.java
@@ -2,7 +2,12 @@ package cars;
 
 public class Kamaz extends Car {
 
-    public Kamaz(Integer speed) {
-        super(speed);
+    public Kamaz(Integer speed, Integer timeInWarehouse) {
+        super(speed, timeInWarehouse);
+    }
+
+    @Override
+    public String toString() {
+        return "Камаз";
     }
 }
