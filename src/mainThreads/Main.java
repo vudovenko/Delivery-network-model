e588d185fad9f24793031b264a1c9bb13e9c2dae
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2023-01-05 14:49:19 +0500

    main

--- src/mainThreads/Main.java
+++ src/mainThreads/Main.java
@@ -53,28 +53,11 @@ public class Main {
         warehouse4.start();
         warehouse5.start();
 
+        Base.sendKamazToWarehouses();
         Ekaterinburg.start();
         NizhnyTagil.start();
         KamenskUralsky.start();
         SatelliteTownsYekaterinburg.start();
-
-
-//        //Вывод нынешней даты и времени
-//        System.out.println("\nДата: " + new Date());
-//        Town Ekaterinburg = new Town(7, randomNumberInRange(10, 30),
-//                1, "Екатерибург");
-//        Town NizhnyTagil = new Town(5, randomNumberInRange(130, 140),
-//                2, "Нижний Тагил");
-//        Town KamenskUralsky = new Town(4, randomNumberInRange(125, 135),
-//                3, "Каменск-Уральский");
-//        Town SatelliteTownsYekaterinburg = new Town(4, randomNumberInRange(10, 30),
-//                4, "спутника Екатеринбурга");
-//        while (true) {
-//            Ekaterinburg.start();
-//            NizhnyTagil.start();
-//            KamenskUralsky.start();
-//            SatelliteTownsYekaterinburg.start();
-//        }
     }
 
     public static Integer randomNumberInRange(Integer min, Integer max) {
