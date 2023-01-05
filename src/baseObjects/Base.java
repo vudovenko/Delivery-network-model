d898fb3dd08bc66868d602701f4d329ec1a0b7a2
Andrey Dikov <DikovAndrey856@gmail.com>
2023-01-05 11:57:58 +0300

    modified findSuitableWarehouse method in Base class

--- src/baseObjects/Base.java
+++ src/baseObjects/Base.java
@@ -31,17 +31,42 @@ public class Base {
     }
 
     public static Warehouse findSuitableWarehouse(TypeProduct typeProduct) {
-        if (typeProduct == TypeProduct.HEAVY_OVERSIZE && warehouses.get(0).getParkingSpace().getParkingSize() >
-                warehouses.get(1).getParkingSpace().getParkingSize()) {
-            return warehouses.get(1);
-        } else if (typeProduct == TypeProduct.HEAVY_OVERSIZE && warehouses.get(0).getParkingSpace().getParkingSize() <
-                warehouses.get(1).getParkingSpace().getParkingSize()) {
-            return warehouses.get(0);
+        if (areThereMoreFreeSpacesInFirstParkingLot(typeProduct)) {
+            return findFirstWarehouseForHeavyOversize();
+        } else if (areThereMoreFreeSpacesInFirstParkingLot(typeProduct)) {
+            return findSecondWarehouseForHeavyOversize();
         } else {
             for (Warehouse warehouse : warehouses) {
                 if (warehouse.getProduct().getTypeProduct().equals(typeProduct)) {
                     return warehouse;
                 }
+            }
+        }
+        return null;
+    }
+
+    private static Boolean areThereMoreFreeSpacesInFirstParkingLot(TypeProduct typeProduct) {
+        return typeProduct == TypeProduct.HEAVY_OVERSIZE &&
+                findFirstWarehouseForHeavyOversize().getParkingSpace().getParkingSize() <
+                        findSecondWarehouseForHeavyOversize().getParkingSpace().getParkingSize();
+    }
+
+    private static Warehouse findFirstWarehouseForHeavyOversize() {
+        for (Warehouse warehouse : warehouses) {
+            if (warehouse.getProduct().getTypeProduct() == TypeProduct.HEAVY_OVERSIZE) {
+                return warehouse;
+            }
+        }
+        return null;
+    }
+
+    private static Warehouse findSecondWarehouseForHeavyOversize() {
+        Integer counter = 0;
+        for (Warehouse warehouse : warehouses) {
+            if (warehouse.getProduct().getTypeProduct() == TypeProduct.HEAVY_OVERSIZE && counter == 1) {
+                return warehouse;
+            } else {
+                counter++;
             }
         }
         return null;
